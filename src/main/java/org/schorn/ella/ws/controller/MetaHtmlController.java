9212bdd27a80544e1e1ee021796b2ed80f43ca02
Bryan Schorn <bschorn@gmail.com>
2019-12-23 00:31:20 -0500

    CheckIn

--- src/main/java/org/schorn/ella/ws/controller/MetaHtmlController.java
+++ src/main/java/org/schorn/ella/ws/controller/MetaHtmlController.java
@@ -3,6 +3,8 @@ package org.schorn.ella.ws.controller;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import org.schorn.ella.services.ActiveServices;
 import org.springframework.stereotype.Component;
@@ -21,21 +23,38 @@ import org.springframework.stereotype.Component;
 //@Api(value = "Meta HTML API")
 public class MetaHtmlController {
 
-    //@Context
-    //private HttpHeaders headers;
+    @Context
+    private HttpHeaders headers;
 
-    /*
-	 * This will return the member types for a object type
+    /**
+     *
+     * @param context
+     * @param object_type
+     * @return
      */
     @GET
-    @Path("/object_type/{context}/{object_type}")
-    public Response getWithContextAndObjectType(
+    @Path("/form/{context}/{object_type}")
+    public Response getHtmlForm(
             @PathParam("context") String context,
             @PathParam("object_type") String object_type) {
         String html = ActiveServices.contentTypeOutput().getHTMLForm(context, object_type);
         return Response.ok(html).build();
     }
 
+    /**
+     *
+     * @param context
+     * @param object_type
+     * @return
+     */
+    @GET
+    @Path("/page/{context}/{object_type}")
+    public Response getHtmlFormInPage(
+            @PathParam("context") String context,
+            @PathParam("object_type") String object_type) {
+        String html = ActiveServices.contentTypeOutput().getHTMLFormInPage(context, object_type);
+        return Response.ok(html).build();
+    }
 }
 
 
